e2e94719e1c6306ac2bfaabb66cb5fdcd98a0ef8
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-06-28 00:17:27 -0500

    finished index.js module

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Catagories.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag_name',
+  foreignKey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tags_name',
+  foreignKey: 'tag_id',
+});
 
+// Export models
 module.exports = {
   Product,
   Category,

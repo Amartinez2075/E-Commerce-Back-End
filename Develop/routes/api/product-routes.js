c5082ab22b883280674b2a2315da127bbe3f0228
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-06-30 23:26:05 -0500

    updated product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -62,28 +62,25 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
- Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    category_id: req.body.category_id,
-    tagIds: req.body.tagIds
-    })
+  Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      // if there are product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
-            tag_id,
-          };
+            tag_id: tag_id
+          }
         });
         return ProductTag.bulkCreate(productTagIdArr);
+      } else {
+        return [];
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((productTagIds) => {
+      // if no product tags, just respond
+      res.status(200).json(productTagIds);
+    })
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
